81999cc1874e7aadc9e0e0d16870629f3991a550
clawfish <glx001@mail.uset.edu.cn>
2024-07-07 13:54:00 +0800

    Fix import errors and library issues
    - ImportError: generic_type: type "matrix" is already registered!
    - ImportError: undefined symbol: cudaMemcpyAsync
    - ImportError: libcusolver.so.11: cannot open shared object file: No such file or directory
    - ImportError: libcudart.so.11.0: cannot open shared object file: No such file or directory
    
    Version updated to 1.0.2.

--- c/cudacore/test/test.cu
+++ c/cudacore/test/test.cu
@@ -5,32 +5,32 @@
 
 
 int main(){
-    matrix *P = read_csv("../../../../data/P.csv", ',');
-    matrix *lb = read_csv("../../../../data/lb.csv", ',');
-    matrix *rb = read_csv("../../../../data/rb.csv", ',');
-    matrix *G0 = read_csv("../../../../data/G0.csv", ',');
-    matrix *h0 = read_csv("../../../../data/h0.csv", ',');
-    matrix *G1 = read_csv("../../../../data/G1.csv", ',');
-    matrix *h1 = read_csv("../../../../data/h1.csv", ',');
-    matrix *G2 = read_csv("../../../../data/G2.csv", ',');
-    matrix *h2 = read_csv("../../../../data/h2.csv", ',');
+    cumatrix *P = read_csv("../../../../data/P.csv", ',');
+    cumatrix *lb = read_csv("../../../../data/lb.csv", ',');
+    cumatrix *rb = read_csv("../../../../data/rb.csv", ',');
+    cumatrix *G0 = read_csv("../../../../data/G0.csv", ',');
+    cumatrix *h0 = read_csv("../../../../data/h0.csv", ',');
+    cumatrix *G1 = read_csv("../../../../data/G1.csv", ',');
+    cumatrix *h1 = read_csv("../../../../data/h1.csv", ',');
+    cumatrix *G2 = read_csv("../../../../data/G2.csv", ',');
+    cumatrix *h2 = read_csv("../../../../data/h2.csv", ',');
     CublasHandle cublas_handle = CublasHandle();
     CusolverHandle cusolver_handle = CusolverHandle();
-    // qp1 - 1
-    qp1 solver1(cublas_handle, cusolver_handle, P, nullptr, lb, rb, G0, h0);
-    matrix *x1 = solver1.solve();
+    // cuqp1 - 1
+    cuqp1 solver1(cublas_handle, cusolver_handle, P, nullptr, lb, rb, G0, h0);
+    cumatrix *x1 = solver1.solve();
     x1->_display(5);
-    // qp1 - 2
-    qp1 solver2(cublas_handle, cusolver_handle, P, nullptr, nullptr, nullptr, G1, h1);
-    matrix *x2 = solver2.solve();
+    // cuqp1 - 2
+    cuqp1 solver2(cublas_handle, cusolver_handle, P, nullptr, nullptr, nullptr, G1, h1);
+    cumatrix *x2 = solver2.solve();
     x2->_display(5); 
-    // qp2 - 3
-    qp2 solver3(cublas_handle, cusolver_handle, P, nullptr, lb, rb, G0, h0);
-    matrix *x3 = solver3.solve();
+    // cuqp2 - 3
+    cuqp2 solver3(cublas_handle, cusolver_handle, P, nullptr, lb, rb, G0, h0);
+    cumatrix *x3 = solver3.solve();
     x3->_display(5);
-    // qp2 - 4
-    qp2 solver4(cublas_handle, cusolver_handle, P, nullptr, nullptr, nullptr, G2, h2);
-    matrix *x4 = solver4.solve();
+    // cuqp2 - 4
+    cuqp2 solver4(cublas_handle, cusolver_handle, P, nullptr, nullptr, nullptr, G2, h2);
+    cumatrix *x4 = solver4.solve();
     x4->_display(5);
 }
 
